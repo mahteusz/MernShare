1870e6c001976cd9dba25f20fb527b7ee112293d
Mateus Marques <mateusmarques200@gmail.com>
2023-02-09 23:48:06 -0300

    adicionando lógica de upload de arquivos

--- client/src/components/Dropzone/index.tsx
+++ client/src/components/Dropzone/index.tsx
@@ -2,14 +2,31 @@ import { useDropzone } from "react-dropzone"
 import * as S from './styled'
 
 const Dropzone = () => {
-  const { getRootProps, getInputProps } = useDropzone({})
+
+  const onDrop = (files: File[]) => {
+    console.log(files)
+  }
+
+  const { getRootProps, getInputProps, acceptedFiles } = useDropzone({ onDrop })
+
 
   return (
     <S.Container {...getRootProps()}>
       <input {...getInputProps()} />
-      <S.Message>
-        Clique aqui ou arraste um arquivo
-      </S.Message>
+      {
+        acceptedFiles.length > 0
+          ? acceptedFiles.map((file: File) => {
+              return (
+                <S.FileInfo>
+                  {`${file.name} - ${(file.size/(1024*1024)).toFixed(2)}MB`}
+                </S.FileInfo>
+              )
+            })
+
+          : <S.Message>
+              Clique aqui ou arraste um arquivo
+            </S.Message>
+      }
     </S.Container>
   )
 }
--- client/src/components/Dropzone/styled.ts
+++ client/src/components/Dropzone/styled.ts
@@ -2,22 +2,30 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   display:flex;
+  flex-direction:column;
   justify-content:center;
+  padding:90px 45px;
   align-items:center;
   background:${props => props.theme.colors.grey};
   border:4px dashed ${props => props.theme.colors.primary};
   transition:background-color 0.3s ease;
+  width:700px;
+  gap:20px;
 
   &:hover {
     cursor:pointer;
   }
 `
 
-export const Message = styled.h1`
+export const Message = styled.span`
   color:${props => props.theme.colors.textColor};
   font-family:${props => props.theme.fonts.primary};
-  font-size:${props => props.theme.fonts.sizes.xlarge};
-  font-weight:bold;  
+  font-size:${props => props.theme.fonts.sizes.medium};
+  font-weight:normal;
   text-align:center;
-  padding:90px 45px;
+  max-width:90%;
+`
+
+export const FileInfo = styled(Message)`
+  color:${props => props.theme.colors.primary};
 `
